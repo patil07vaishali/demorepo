9e74c49df36ecfaeccb10dfd5471b37a0cf87f2e
patil07vaishali <patilvaishu07@gmail.com>
2025-01-24 16:08:15 +0530

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,14 @@
+ echo "BUILD_NUMBER" :: $BUILD_NUMBER
+ echo "BUILD_ID" :: $BUILD_ID
+ echo "BUILD_DISPLAY_NAME" :: $BUILD_DISPLAY_NAME
+ echo "JOB_NAME" :: $JOB_NAME
+ echo "WORKSPACE" :: $WORKSPACE
+ echo "JENKINS_HOME" :: $JENKINS_HOME
+ echo "JENKINS_URL" :: $JENKINS_URL
+ echo "BUILD_URL" ::$BUILD_URL
+ echo "JOB_URL" :: $JOB_URL
+ echo "GIT_COMMIT" :: $GIT_COMMIT
+ echo "GIT_BRANCH" :: $GIT_BRANCH
+ echo "GIT_URL" :: $GIT_URL
+ echo "Below output is all the environment variable in Jenkins"
+ printenv
